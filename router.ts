45830d4bf9d03fa8ee5b699deaa84e522226bf02
Darwin Salinas <salinash2000@gmail.com>
2022-09-30 21:24:17 -0600

    Primeras rutas con controller y repository

--- router.ts
+++ router.ts
@@ -1,13 +1,39 @@
 import { Router, Request, Response, NextFunction, Express } from 'express';
+import controller from './controller';
 
 export const appRouter = (app: Express) => {
     // const router = Router();
 
-    app.get('/', (req: Request, res: Response, next: NextFunction) => {
-        res.json({ 'message': 'hola mundo' });
+    app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+        const response = await controller.root();
+
+        res.json(response);
     });
 
-    app.get('/hola', (req: Request, res: Response, next: NextFunction) => {
-        res.json({ 'message': 'hola' });
+    app.get('/hola', async (req: Request, res: Response, next: NextFunction) => {
+        const response = await controller.greetings();
+
+        res.json(response);
     });
+
+
+    app.get('/products', async (req: Request, res: Response) => {
+        try {
+            const response = await controller.getProducts();
+
+            res.json(response);
+        } catch (error) {
+            res.json({ message: 'Whoops something went wrong' })
+        }
+    });
+
+    app.get('/products/:id', async (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        const response = await controller.getProduct(Number(id));
+        console.log(response);
+
+
+        res.json(response);
+    })
 }
