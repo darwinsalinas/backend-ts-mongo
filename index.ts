005f5965f04a813a4e9a3f59c6b7ec5d17a26891
Darwin Salinas <salinash2000@gmail.com>
2022-09-30 19:47:59 -0600

    Primeras rutas con express

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
-import cors from 'cors';
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-    console.log('Codigo Poque sos tan bonito, poque , poque, poque');
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.json({ 'message': 'hola mundo' });
 });
 
-
+app.get('/hola', (req, res, next) => {
+    res.json({ 'message': 'hola' });
+});
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
